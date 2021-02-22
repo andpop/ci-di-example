6d99cadf74a605042a05dd4a484e07cf6e702819
Andrey Popov <andpop@mail.ru>
2021-02-22 10:20:25 +0300

    Fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import React from "React";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
-  expect(false).toEqual(true);
+  // expect(false).toEqual(true);
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
@@ -15,4 +15,4 @@ it("renders without crashing", () => {
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
-//comment
+//comment2
